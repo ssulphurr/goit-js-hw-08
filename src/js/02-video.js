47c4982d643d423629f568e11d20682376e179cd
ssulphurr <ssulphurr@gmail.com>
2023-03-07 00:29:44 +0200

    adds working timeupdate to task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+// copied from documentation to vimeo
 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', function (evt) {
+  console.log(evt);
+
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+});
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
