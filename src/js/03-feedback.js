826ec8811b2f430e273ab6edbb302facdfbea1ac
ssulphurr <ssulphurr@gmail.com>
2023-03-07 01:56:26 +0200

    adds raw task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const formEl = document.querySelector('.feedback-form');
 
+// //////////////////// trying to make with listener on form: unsuccessful
+
+// const formFilling = {
+//   email: formEl.elements.email.value,
+//   message: formEl.elements.message.value,
+// };
+
+// function saveToStorage(evt) {
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formFilling));
+// }
+
+// formEl.addEventListener('input', saveToStorage);
+
+// const savedFilling = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+// console.log(savedFilling);
+
+// //////////////////// variant with only intup fild working
+
+const inputEl = formEl.elements.email;
+const textareaEl = formEl.elements.message;
+
+inputEl.addEventListener('input', saveToStorage);
+
+function saveToStorage(evt) {
+  localStorage.setItem('feedback-form-state', evt.currentTarget.value);
+}
+
+console.log(localStorage.getItem('feedback-form-state'));
+inputEl.value = localStorage.getItem('feedback-form-state');
