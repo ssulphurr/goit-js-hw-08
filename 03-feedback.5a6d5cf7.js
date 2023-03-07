87943b69ac9dbe8baf0fe86e3a9781f500302895
ssulphurr <114026438+ssulphurr@users.noreply.github.com>
2023-03-07 18:32:57 +0000

    Deploying to gh-pages from @ ssulphurr/goit-js-hw-08@9d582cd89186e97b10c9b1a08bbcbfb5b4afc20d 🚀

--- 03-feedback.5a6d5cf7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=e.elements.email,a=e.elements.message,o={email:t.value,message:a.value};function l(e){localStorage.setItem("feedback-form-state",JSON.stringify(o))}t.addEventListener("input",l),console.log(JSON.parse(localStorage.getItem("feedback-form-state"))),a.addEventListener("input",l),console.log(localStorage.getItem("feedback-form-state")),a.value=localStorage.getItem("feedback-form-state")}();
-//# sourceMappingURL=03-feedback.5a6d5cf7.js.map
--- 03-feedback.fdf660cc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=e.elements.email,a=e.elements.message,o={email:t.value,message:a.value};function l(e){localStorage.setItem("feedback-form-state",JSON.stringify(o))}t.addEventListener("input",l),console.log(JSON.parse(localStorage.getItem("feedback-form-state"))),a.addEventListener("input",l),console.log(localStorage.getItem("feedback-form-state")),a.value=localStorage.getItem("feedback-form-state");
-//# sourceMappingURL=03-feedback.fdf660cc.js.map
